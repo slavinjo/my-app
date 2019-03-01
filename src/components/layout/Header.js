464a6e71e9ca94f73c218acff2037a142820507f
Slaven Robic <slaven.robic@gmail.com>
2019-03-01 15:53:13 +0100

    klik

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from "../../css/Header.module.css";
+import Logo from '../../components/layout/Logo';
 
 const Header = ( props ) => (
     <header className={classes.Header}>
+       <Logo/> 
        <p>{props.title}</p>
     </header>
 );
--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -3,9 +3,14 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from '../../css/Logo.module.css';
 
+const clickHandler = () => {
+    alert('mater');
+}
+
+
 const Logo = (props) => (
     <div className={classes.Logo} style={{height: props.height}}>
-        <img src={burgerLogo} alt="MyBurger" />
+        <img src={burgerLogo} alt="MyBurger"  onClick={clickHandler}  />
     </div>
 );
 
